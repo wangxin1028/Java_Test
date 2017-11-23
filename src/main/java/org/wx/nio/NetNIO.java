e68d10450ebe4629512c6e4128a22ed9dbe51103
wangxin <wangxin0110@hotmail.com>
2017-11-23 20:24:59 +0800

    update

--- src/main/java/org/wx/nio/NetNIO.java
+++ src/main/java/org/wx/nio/NetNIO.java
@@ -1,5 +1,57 @@
 package org.wx.nio;
 
-public class NetNIO {
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
 
+import org.junit.Test;
+
+public class NetNIO {
+	@Test
+	public void testBlockClient() {
+		try {
+			SocketChannel channel = SocketChannel.open(new InetSocketAddress("127.0.0.1", 7379));
+			String s = "我是客户端";
+			ByteBuffer buffer = ByteBuffer.allocate(1024);
+			buffer.put(s.getBytes());
+			buffer.flip();
+			channel.write(buffer);
+			
+			buffer.clear();
+			channel.read(buffer);
+			buffer.flip();
+			System.out.println(new String(buffer.array(),0,buffer.limit()));
+			channel.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	@Test
+	public void testBlockServer() {
+		try {
+			ServerSocketChannel server = ServerSocketChannel.open();
+			server.bind(new InetSocketAddress(7379));
+			
+			SocketChannel accept = server.accept();
+			ByteBuffer buffer = ByteBuffer.allocate(1024);
+			accept.read(buffer);
+			
+			buffer.flip();
+			System.out.println(new String(buffer.array(),0,buffer.limit()));
+			
+			buffer.clear();
+			buffer.put("我是服务端".getBytes());
+			buffer.flip();
+			accept.write(buffer);
+			
+			accept.close();
+			server.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
