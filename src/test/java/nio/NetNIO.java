e0a89aa55541b37fa68dce7c31d0def11d1a16c3
wangxin <wangxin0201@outlook.com>
2017-11-25 20:58:41 +0800

    update

--- src/test/java/nio/NetNIO.java
+++ src/test/java/nio/NetNIO.java
@@ -9,106 +9,112 @@ import java.util.Iterator;
 import org.junit.Test;
 
 public class NetNIO {
-	@Test
-	public void testNoBlockClient() {
-		try {
-			SocketChannel channel = SocketChannel.open(new InetSocketAddress("127.0.0.1", 8888));
-			channel.configureBlocking(false);
+    @Test
+    public void testNoBlockClient() {
+        try {
+            SocketChannel channel = SocketChannel.open(new InetSocketAddress("127.0.0.1", 8888));
+            channel.configureBlocking(false);
 
-			String s = "a";
-			ByteBuffer buffer = ByteBuffer.allocate(1024);
-			buffer.put(s.getBytes());
-			buffer.flip();
-			channel.write(buffer);
-			channel.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	@Test
-	public void testNoBlockServer() {
-		try {
-			ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
-			serverSocketChannel.bind(new InetSocketAddress(8888));
+            String s = "a";
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+            buffer.put(s.getBytes());
+            buffer.flip();
+            channel.write(buffer);
+            channel.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
-			serverSocketChannel.configureBlocking(false);
+    @Test
+    public void testNoBlockServer() {
+        try {
+            ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+            serverSocketChannel.bind(new InetSocketAddress(8888));
 
-			Selector selector = Selector.open();
+            serverSocketChannel.configureBlocking(false);
 
-			serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+            Selector selector = Selector.open();
 
-			while(selector.select()>0) {
-				Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
-				while(iterator.hasNext()) {
-					SelectionKey selectionKey = iterator.next();
-					if(selectionKey.isAcceptable()) {
-						//接收准备就绪
-						SocketChannel socketChannel = serverSocketChannel.accept();
-						socketChannel.configureBlocking(false);
-						socketChannel.register(selector, SelectionKey.OP_READ);
-					}else if(selectionKey.isReadable()) {
+            serverSocketChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+            while (selector.select() > 0) {
+                Iterator<SelectionKey> iterator = selector.selectedKeys().iterator();
+                while (iterator.hasNext()) {
+                    SelectionKey selectionKey = iterator.next();
+                    if (selectionKey.isAcceptable()) {
+                        //接收准备就绪
+                        SocketChannel socketChannel = serverSocketChannel.accept();
+                        socketChannel.configureBlocking(false);
+                        socketChannel.register(selector, SelectionKey.OP_READ);
+                    } else if (selectionKey.isReadable()) {
                         SocketChannel channel = (SocketChannel) selectionKey.channel();
                         ByteBuffer buffer = ByteBuffer.allocate(1024);
 
-                        while(channel.read(buffer)>0){
+
+                        if (channel.read(buffer) > 0) {
+                            System.out.println("有数据");
+                            channel.read(buffer);
                             buffer.flip();
-                            System.out.println(new String(buffer.array(),0,buffer.limit()));
+                            System.out.println(new String(buffer.array(), 0, buffer.limit()));
                         }
                     }
-					iterator.remove();
-				}
-			}
-			serverSocketChannel.close();
+                    iterator.remove();
+                }
+            }
+            serverSocketChannel.close();
 
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	@Test
-	public void testBlockClient() {
-		try {
-			SocketChannel channel = SocketChannel.open(new InetSocketAddress("127.0.0.1", 7379));
-			String s = "我是客户端";
-			ByteBuffer buffer = ByteBuffer.allocate(1024);
-			buffer.put(s.getBytes());
-			buffer.flip();
-			channel.write(buffer);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
-			buffer.clear();
-			channel.read(buffer);
-			buffer.flip();
-			System.out.println(new String(buffer.array(),0,buffer.limit()));
-			channel.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	@Test
-	public void testBlockServer() {
-		try {
-			ServerSocketChannel server = ServerSocketChannel.open();
-			server.bind(new InetSocketAddress(7379));
+    @Test
+    public void testBlockClient() {
+        try {
+            SocketChannel channel = SocketChannel.open(new InetSocketAddress("127.0.0.1", 7379));
+            String s = "我是客户端";
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+            buffer.put(s.getBytes());
+            buffer.flip();
+            channel.write(buffer);
 
-			SocketChannel accept = server.accept();
-			ByteBuffer buffer = ByteBuffer.allocate(1024);
-			accept.read(buffer);
+            buffer.clear();
+            channel.read(buffer);
+            buffer.flip();
+            System.out.println(new String(buffer.array(), 0, buffer.limit()));
+            channel.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
-			buffer.flip();
-			System.out.println(new String(buffer.array(),0,buffer.limit()));
+    @Test
+    public void testBlockServer() {
+        try {
+            ServerSocketChannel server = ServerSocketChannel.open();
+            server.bind(new InetSocketAddress(7379));
 
-			buffer.clear();
-			buffer.put("我是服务端".getBytes());
-			buffer.flip();
-			accept.write(buffer);
+            SocketChannel accept = server.accept();
+            ByteBuffer buffer = ByteBuffer.allocate(1024);
+            accept.read(buffer);
 
-			accept.close();
-			server.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+            buffer.flip();
+            System.out.println(new String(buffer.array(), 0, buffer.limit()));
+
+            buffer.clear();
+            buffer.put("我是服务端".getBytes());
+            buffer.flip();
+            accept.write(buffer);
+
+            accept.close();
+            server.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 }
