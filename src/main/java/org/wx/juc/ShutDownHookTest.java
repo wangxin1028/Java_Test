3584143ead2aa80b101866bcc081520b3f838689
wangxin <wangxin0201@outlook.com>
2017-11-26 12:24:19 +0800

    Merge branch 'master' of https://github.com/wangxin1028/YongLe
    # Conflicts:
    #	src/test/java/nio/TCPTest.java

---
+++ src/main/java/org/wx/juc/ShutDownHookTest.java
@@ -0,0 +1,20 @@
+package org.wx.juc;
+
+public class ShutDownHookTest {
+    public static void main(String[] args) {
+        Runtime.getRuntime().addShutdownHook(new ShutDownAction());
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
+class ShutDownAction extends Thread{
+    @Override
+    public void run() {
+        System.out.println("程序退出");
+    }
+}
