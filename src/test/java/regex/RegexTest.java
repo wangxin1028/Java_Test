e1c0cc0eece78cf1503d2883057d3573e08a0649
wangxin <wangxin0110@hotmail.com>
2017-12-12 17:07:39 +0800

    update

--- src/test/java/regex/RegexTest.java
+++ src/test/java/regex/RegexTest.java
@@ -1,10 +1,12 @@
 package regex;
 
+import java.io.UnsupportedEncodingException;
+
 import org.junit.Test;
 
 public class RegexTest {
 	@Test
-	public void test() {
+	public void test() throws UnsupportedEncodingException {
 		String s = "http://www.baidu.com\\";
 		String r = s.replaceAll("\\+", "%20");
 		System.out.println(r);
