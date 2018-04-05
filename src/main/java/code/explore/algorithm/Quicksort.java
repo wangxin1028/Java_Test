4df87257784ca97255aa5d0da0d2df0667a61af4
wangxin <wangxin0201@outlook.com>
2018-04-05 16:04:42 +0800

    快速排序

---
+++ src/main/java/code/explore/algorithm/Quicksort.java
@@ -0,0 +1,48 @@
+package code.explore.algorithm;
+
+import org.omg.CORBA.IRObject;
+
+public class Quicksort {
+    public static void sort(int[] array){
+        quickSort(array,0,array.length-1);
+    }
+
+    private static void quickSort(int[] array,int left,int right){
+       if(left<right){
+           int middle = unitSort(array, left, right);
+           quickSort(array,left,middle-1);
+           quickSort(array,middle+1, right);
+       }
+       return;
+    }
+
+    private static int unitSort(int[] array,int left,int right){
+        int flag=array[left];
+        while(left<right){
+            while(left<right){
+                if(array[right]<flag){
+                    array[left]=array[right];
+                    break;
+                }
+                right--;
+            }
+            while(left<right){
+                if(array[left]>=flag){
+                    array[right]=array[left];
+                    break;
+                }
+                left++;
+            }
+        }
+        array[right]=flag;
+        return right;
+    }
+    
+    public static void main(String[] args) {
+        int[] array = {5,6,3,1,8,4,9,0,23,6};
+        sort(array);
+        for(int i = 0 ; i < array.length ;i++){
+            System.out.println(array[i]);
+        }
+    }
+}
